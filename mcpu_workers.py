9abad1b95b692e4f5cdf97b724e56a69b695f9e9
Florin Andrei <florin@andrei.myip.org>
2019-10-15 19:21:28 -0700

    GPU CUDA via cupy

--- mcpu_workers.py
+++ mcpu_workers.py
@@ -1,4 +1,10 @@
-import numpy as np
+gpu = True
+try:
+    import cupy as xp
+except:
+    import numpy as xp
+    gpu = False
+
 import gc
 
 def make_dots(arglist):
@@ -11,19 +17,22 @@ def make_dots(arglist):
     # # is over 500 MB, or 60 million elements (points * dims).
     # # Half of RAM is taken by pts arrays in all workers.
     # # TODO: implement code to fragment this array when too big
-    pts = np.random.random_sample((points, d)) - 0.5
+    pts = xp.random.random_sample((points, d)) - 0.5
     # keep a sample of points
     ssize = int(100 / num_p)
     pts_sample = pts[:ssize, :]
     # calculate distances from center
-    pts = np.power(pts, 2)
-    dists = np.sum(pts, axis = 1)
+    pts = xp.power(pts, 2)
+    dists = xp.sum(pts, axis = 1)
     del pts
     gc.collect()
     ## square root of sum - it's the distance to origin
-    dists = np.power(dists, 0.5)
+    dists = xp.power(dists, 0.5)
     # this many are within the sphere (distance to origin  <= 0.5)
     p_int = (dists <= 0.5).sum()
     del dists
     gc.collect()
-    return [p_int, pts_sample]
+    if gpu:
+        return [xp.asnumpy(p_int), xp.asnumpy(pts_sample)]
+    else:
+        return [p_int, pts_sample]
