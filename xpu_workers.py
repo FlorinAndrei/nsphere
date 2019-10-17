5859cd6825fdbd8275cd789de1b602a7d00afe32
Florin Andrei <florin@andrei.myip.org>
2019-10-17 16:46:00 -0700

    detect usable memory and split arrays if needed to fit

--- xpu_workers.py
+++ xpu_workers.py
@@ -3,13 +3,17 @@ import random
 
 def make_dots(arglist):
     # multiprocessing.pool.map() only works with one argument
-    points, d, num_p, sysmem, gpumem, pointloops = arglist
+    # so we pass a tuple of arguments as "one argument"
+    points, d, workers, sysmem, gpumem = arglist
+
     if gpumem > 0:
         import cupy as xp
+        # use all the GPU memory
         usemem = gpumem
     else:
         import numpy as xp
-        usemem = sysmem / 2
+        # on a laptop don't use all the system memory
+        usemem = int(sysmem * 0.5)
         # If you use multiprocessing.set_start_method('spawn') (the default on Windows)
         # then the following lines are not necessary. If you use the 'fork' method instead
         # (default on Unix) then all workers will generate identical "random" sequences,
@@ -17,39 +21,65 @@ def make_dots(arglist):
         # as shown here:
         #rseed = random.randint(0, 4294967296)
         #xp.random.seed(rseed)
-    points = int(points / num_p)
-    # generate all points
-    # # on a system with 16 GB RAM and 12 CPUs / workers,
-    # # system memory is exhausted when the pts array
-    # # is over 500 MB, or 60 million elements (points * dims).
-    # # Half of RAM is taken by pts arrays in all workers.
-    # # TODO: implement code to fragment this array when too big
 
-    # 0.04 is a magic number based on
+    # if workers > 1, we have more than one worker
+    # tell the worker to divide its share of calculations
+    points = int(points / workers)
+
+    # this is a magic number based on
     # the size of each cell in the pts matrix (float32 usually)
-    magic_ratio = 0.04
-    if (points * d / usemem) > magic_ratio:
-        split = True
+    # TODO: compute magic_ratio from array type
+    #       calculate array size, then use a percentage instead of magic_ratio
+    magic_ratio = 0.03
 
+    # too many points and too many dimensions - it won't fit in memory
+    # so then split the job in multiple worker_loops
+    # each loop ought to fit in memory (see magic_ratio)
+    data_vs_mem = points * d * workers / usemem
+    if data_vs_mem > magic_ratio:
+        split = True
+        worker_loops = int(data_vs_mem / magic_ratio) + 1
+        points_per_loop = int(points / worker_loops)
+        final_loop_points = points - points_per_loop * (worker_loops - 1)
+        points = points_per_loop
+    else:
+        split = False
+        worker_loops = 1
+        points_per_loop = points
+        final_loop_points = 0
+
+    # initialize number of points inside the sphere
     p_int = 0
-    for i in range(pointloops):
-        pts = xp.random.random_sample((points, d)) - 0.5
-        # keep a sample of points
-        ssize = int(100 / num_p)
-        pts_sample = pts[:ssize, :]
-        # calculate distances from center
+
+    for i in range(worker_loops):
+        if split and i == worker_loops - 1 and final_loop_points != 0:
+            # this is the last loop and the remainder is not zero
+            # TODO: the remainder should never be zero - seems like logic bug
+            pts = xp.random.random_sample((final_loop_points, d)) - 0.5
+        else:
+            pts = xp.random.random_sample((points_per_loop, d)) - 0.5
+        if i == 0:
+            # set aside a sample of points for display
+            # total sample size from all workers ~= 100 points
+            ssize = int(100 / workers)
+            pts_sample = pts[:ssize, :]
+
+        # calculate distances-squared from center
         pts = xp.power(pts, 2)
         dists = xp.sum(pts, axis = 1)
         del pts
         gc.collect()
+
         ## square root of sum - it's the distance to origin
         dists = xp.power(dists, 0.5)
-        # this many are within the sphere (distance to origin  <= 0.5)
-        p_int += (dists <= 0.5).sum()
+        # this many points are within the sphere (distance to origin  <= 0.5)
+        p_int_sum = (dists <= 0.5).sum()
+        p_int = p_int + p_int_sum
         del dists
         gc.collect()
-    p_int = p_int / pointloops
+
+    #p_int = p_int / worker_loops
     if gpumem > 0:
-        return [xp.asnumpy(p_int), xp.asnumpy(pts_sample)]
+        return [p_int, xp.asnumpy(pts_sample)]
     else:
         return [p_int, pts_sample]
